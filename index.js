29086ded427b1689bb3536b0fa9b765416cfb914
Flavio Fernandes Junior <100242594+flaviofernandesjunior@users.noreply.github.com>
2022-08-24 21:13:16 -0300

    alterações no exercicio

--- index.js
+++ index.js
@@ -1,7 +1,25 @@
 function getUserInfo() {
     fetch("https://randomuser.me/api/")
     .then(response => response.json())
-    .then(response => console.log(response))
+    .then((response) => {
+        let usuario = response.results[0]
+        let primeiroNome = document.getElementById("full-name")
+        primeiroNome.innerHTML = usuario.name.first + " " + usuario.name.last;
+        console.log(usuario)
+        let oEmail = document.getElementById("email")
+        oEmail.innerHTML = usuario.email;
+    }
+    )
+    .catch((error) => {
+        console.log(error);
+    })
+    .finally(() => {
+        console.log("Usuario carregado");
+    })
 }
 
 getUserInfo();
+
+
+
+
