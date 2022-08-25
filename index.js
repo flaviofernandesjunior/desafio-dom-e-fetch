d15c6acedf525a2f98425e08dfb12dbae5c54991
Flavio Fernandes Junior <100242594+flaviofernandesjunior@users.noreply.github.com>
2022-08-24 23:33:58 -0300

    manipulando dom

--- index.js
+++ index.js
@@ -3,12 +3,18 @@ function getUserInfo() {
     .then(response => response.json())
     .then((response) => {
         let usuario = response.results[0]
-        let primeiroNome = document.getElementById("full-name")
+        let primeiroNome = document.getElementById("full-name");
         primeiroNome.innerHTML = usuario.name.first + " " + usuario.name.last;
         console.log(usuario)
-        let oEmail = document.getElementById("email")
+        let oEmail = document.getElementById("email");
         oEmail.innerHTML = usuario.email;
-    }
+        let numtelefone = document.getElementById("phone");
+        numtelefone.innerHTML = usuario.phone;
+        let nomerua = document.getElementById("street");
+        nomerua.innerHTML = usuario.location.street.name;
+        let nomeEstado = document.getElementById("state");
+        nomeEstado.innerHTML = usuario.location.state;
+        }
     )
     .catch((error) => {
         console.log(error);
